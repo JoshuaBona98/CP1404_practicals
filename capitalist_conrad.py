503bf29a86eed07aee3e79cc2a05bdcc6ab0ddc7
joshuabona <joshua.bona@my.jcu.edu.au>
2018-03-27 21:35:47 +1000

    Add all practicals

---
+++ capitalist_conrad.py
@@ -0,0 +1,19 @@
+import random
+
+MAX_INCREASE = 0.1
+MAX_DECREASE = 0.05
+MIN_PRICE = 0.01
+MAX_PRICE = 1000
+INITIAL_PRICE = 10
+
+price = INITIAL_PRICE
+print("Starting price is :${:,.2f}".format(price))
+
+while price >= MIN_PRICE and price <= MAX_PRICE:
+    price_change = 0
+    if random.randint(1, 2) == 1:
+        price_change = random.uniform(0, MAX_INCREASE)
+    else:
+        price_change = random.uniform(-MAX_DECREASE, 0)
+    price *= (1 + price_change)
+    print("On day {} price is :${:,.2f}".format(+1,price))
